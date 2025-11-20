16cd64acac59d91c0798575b131097881f23b717
kangrim choi <rkdfla100@gmail.com>
2025-11-20 16:15:07 +0900

    feat: [DELETE] 사용자 정보 삭제

---
+++ app/routes/users.py
@@ -0,0 +1,64 @@
+from flask import Blueprint, request
+from app.utils import build_response, user_not_found_response
+
+# 블루 프린트 정의
+user_bp = Blueprint('user', __name__, url_prefix='/users')
+
+#간단한 API 구현을 위한 인메모리 데이터 저장소 (DB 대용)
+user_db = {
+    "1": {
+        "id": "1",
+        "name": "John Doe",
+        "email": "john@naver.com"
+    }
+}
+
+#POST1 사용자 생성
+@user_bp.route('/', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    if not body or 'id' not in body or 'name' not in body:
+        return build_response(status="fail", code=400, message="Invalid request body")
+    
+    user_id = body['id']
+    if user_id in user_db:
+        return build_response(status="fail", code=400, message="User already exists")
+    
+    user_db[user_id] = body
+    return build_response(status="success", code=201, message="User created", data=user_db)
+
+
+#GET1 사용자 목록 조회
+@user_bp.route('/', methods=['GET'])
+def get_users():
+    if not user_db:
+        return build_response(status="fail", code=404, message="No users found")
+    return build_response(status="success", code=200, message="Users retrived", data=list(user_db.values()))
+
+#GET2 특정 사용자 조회
+@user_bp.route('/<user_id>', methods=['GET'])
+def get_user(user_id):
+    if user_id not in user_db:
+        return user_not_found_response()
+    return build_response(status="success", code=200, message="User retrived", data=user_db[user_id])
+
+#PUT1 사용자 정보 수정
+@user_bp.route('/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    if user_id not in user_db:
+        return user_not_found_response()
+    
+    body = request.get_json()
+    user_db[user_id].update(body)
+    return build_response(status="success", code=200, message="User updated", data=user_db[user_id])
+
+#DELETE1 사용자 정보 삭제
+@user_bp.route('/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    if user_id not in user_db:
+        return user_not_found_response()
+    
+    del user_db[user_id]
+    return build_response(status="success", code=204, message="User deleted")
+
+        
